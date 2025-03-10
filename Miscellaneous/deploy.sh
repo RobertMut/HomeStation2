398da244ee30d1f478169c0a1c09291a566b6f3e
Robert Mutniański <robert.mutnianski@inetum.com>
2025-03-10 12:06:16 +0100

    Add Ingress configuration and update deployment files

--- Miscellaneous/deploy.sh
+++ Miscellaneous/deploy.sh
@@ -1,4 +1,5 @@
 ﻿#!/bin/bash
 
 echo "Deploying homestation"
+kubectl apply -f ./ingress-nginx.yaml
 kubectl apply -f ./homestationdb-deployment.yaml,./homestationapi-deployment.yaml,./homestationweb-deployment.yaml -n homestation
