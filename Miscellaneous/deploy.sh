b67a43a7fa0dcd5992b072b7eb54f1adfc8ad758
Robert Mutniański <robert.mutnianski@inetum.com>
2025-03-11 14:09:28 +0100

    Remove ingress config map deployment from deploy script

--- Miscellaneous/deploy.sh
+++ Miscellaneous/deploy.sh
@@ -1,5 +1,4 @@
 ﻿#!/bin/bash
 
 echo "Deploying homestation"
-kubectl apply -f ./ingress-nginx.yaml
 kubectl apply -f ./homestationdb-deployment.yaml,./homestationapi-deployment.yaml,./homestationweb-deployment.yaml -n homestation
